b1a16de0bbbc089703feca6094da52b51c9c1e0f
Alex Boelens <a.boelens@live.nl>
2020-10-21 06:59:05 +0200

    added remove item functionality

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -9,12 +9,21 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 		payload: {
 			product: data._id,
 			name: data.name,
-			image: data.price,
+			image: data.image,
 			price: data.price,
 			countInStock: data.countInStock,
 			qty
 		}
 	})
 
-	localStorage.setItem('cartitems', JSON.stringify(getState().cart.cartItems))
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+	dispatch({
+		type: CART_REMOVE_ITEM,
+		payload: id
+	})
+
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
