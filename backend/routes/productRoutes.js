ff5bf7187b729b711a81bfdaa2580ae06b40b340
Alex Boelens <a.boelens@live.nl>
2020-11-17 00:53:52 +0100

    added backend routes for creating a review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,15 @@ import {
 	getProductById,
 	deleteProduct,
 	updateProduct,
-	createProduct
+	createProduct,
+	createProductReview
 } from '../controllers/productController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router
 	.route('/:id')
 	.get(getProductById)
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import morgan from 'morgan'
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -14,6 +15,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
@@ -34,7 +39,7 @@ app.get('/api/config/paypal', (req, res) =>
 
 // Middleware
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))	
 
 app.use(notFound)
 app.use(errorHandler)
