4f431e77faf981cf93aeb19b86bbccf86815259b
Alex Boelens <a.boelens@live.nl>
2020-11-18 14:57:20 +0100

    added pagination to homescreen and admin product screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,20 +3,22 @@ import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 import { Row, Col } from 'react-bootstrap'
 import { listProducts } from '../redux/actions/productActions'
 
 const HomeScreen = ({ match }) => {
 	const keyword = match.params.keyword
+	const pageNumber = match.params.pageNumber || 1
 
 	const dispatch = useDispatch()
 
 	const productList = useSelector((state) => state.productList)
-	const { loading, error, products } = productList
+	const { loading, error, products, page, pages } = productList
 
 	useEffect(() => {
-		dispatch(listProducts(keyword))
-	}, [dispatch, keyword])
+		dispatch(listProducts(keyword, pageNumber))
+	}, [dispatch, keyword, pageNumber])
 
 	return (
 		<div>
@@ -26,14 +28,21 @@ const HomeScreen = ({ match }) => {
 			) : error ? (
 				<Message variant='danger'>{error}</Message>
 			) : (
-				<Row>
-					{!loading &&
-						products.map((product) => (
-							<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-								<Product product={product} />
-							</Col>
-						))}
-				</Row>
+				<>
+					<Row>
+						{!loading &&
+							products.map((product) => (
+								<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+									<Product product={product} />
+								</Col>
+							))}
+					</Row>
+					<Paginate
+						page={page}
+						pages={pages}
+						keyword={keyword ? keyword : ''}
+					/>
+				</>
 			)}
 		</div>
 	)
